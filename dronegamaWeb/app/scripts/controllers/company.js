a147a6c9ea10634d946c7f9b9c58eadd352e5839
sidgyl <sid_gyl@yahoo.co.in>
2016-09-10 23:56:23 -0400

    service added

---
+++ dronegamaWeb/app/scripts/controllers/company.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dronegamaWebApp.controller:CompanyCtrl
+ * @description
+ * # CompanyCtrl
+ * Controller of the dronegamaWebApp
+ */
+angular.module('dronegamaWebApp')
+  .controller('CompanyCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
