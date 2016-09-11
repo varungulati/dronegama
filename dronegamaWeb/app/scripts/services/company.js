779a38f8a3db55e5614c942ebc4834a63253919e
sidgyl <sid_gyl@yahoo.co.in>
2016-09-10 23:57:04 -0400

    adding company.js

---
+++ dronegamaWeb/app/scripts/services/company.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name dronegamaWebApp.company
+ * @description
+ * # company
+ * Service in the dronegamaWebApp.
+ */
+angular.module('dronegamaWebApp')
+  .service('company', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
