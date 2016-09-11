49d5e5aee69e2c9662926b7467df3b7163d1eb81
Varun Gulati <varungulati123@gmail.com>
2016-09-10 21:36:30 -0400

    Adding scripts styles and git ignore

---
+++ dronegamaWeb/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dronegamaWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the dronegamaWebApp
+ */
+angular.module('dronegamaWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ dronegamaWeb/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dronegamaWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dronegamaWebApp
+ */
+angular.module('dronegamaWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
