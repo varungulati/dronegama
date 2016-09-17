f2671034898ca8b5a27fd10065b1fe25d549d9a2
sidgyl <sid_gyl@yahoo.co.in>
2016-09-17 19:01:58 -0400

    removed dist static url

--- dronegama/settings.py
+++ dronegama/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_ROOT = '/dist/'
+# STATIC_ROOT = '/dist/'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
