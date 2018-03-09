e5ab1ba4cbdb886042f828a747ca4874e00d539e
Varun Gulati <varungulati123@gmail.com>
2018-03-08 19:55:36 -0500

    Removing localhost from service

--- dronegamaWeb/app/scripts/services/ticker.js
+++ dronegamaWeb/app/scripts/services/ticker.js
@@ -11,7 +11,7 @@ angular.module('dronegamaWebApp').factory('ticker', function($http) {
 	  var myService = {
 	    async: function() {
 	      // $http returns a promise, which has a then function, which also returns a promise
-	      var promise = $http.get('http://localhost:8000/ticker/').then(function (response) {
+	      var promise = $http.get('/ticker/').then(function (response) {
 	        // The then function here is an opportunity to modify the response
 	        console.log(response);
 	        // The return value gets picked up by the then in the controller.
