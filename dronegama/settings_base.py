21be8f01a4c9f6294ded7661f36d670d99a13c58
Varun Gulati <varungulati123@gmail.com>
2018-07-28 22:58:44 -0700

    Removing validations from password

--- dronegama/settings_base.py
+++ dronegama/settings_base.py
@@ -97,18 +97,6 @@ WSGI_APPLICATION = 'dronegama.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
 ]
 
 
