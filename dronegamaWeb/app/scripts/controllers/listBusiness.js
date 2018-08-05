128ed1ebb97288afe37e42b89b0f6c8d361e8821
Varun Gulati <varungulati123@gmail.com>
2018-08-04 23:46:34 -0700

    Adding list your business flow

--- dronegamaWeb/app/scripts/controllers/listBusiness.js
+++ dronegamaWeb/app/scripts/controllers/listBusiness.js
@@ -8,9 +8,17 @@
  * Controller of the dronegamaWebApp
  */
 angular.module('dronegamaWebApp')
-  .controller('ListBusinessCtrl', function ($scope) {
+  .controller('ListBusinessCtrl', function ($timeout, $scope) {
     if(!$scope.main.isAuthenticated) {
-      $('#myModal').modal('show');
+      $('#notLoggedInModal').modal('show');
+    } else {
+      $('#listBusinessModalStart').modal('show');
+      $timeout(function() {
+        $('#listBusinessModalStart').modal('hide');
+        $('#listBusinessModalSteps').modal('show');
+      }, 3000);
+
+
     }
   });
 
