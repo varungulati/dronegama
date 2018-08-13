8379664fc37a5f112362357543d7417d84f1fed3
Varun Gulati <varungulati123@gmail.com>
2018-08-12 17:45:24 -0700

    Upgrading django to 1.11.15

--- listbusiness/models.py
+++ listbusiness/models.py
@@ -1,11 +1,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+
+
 
 class Business(models.Model):
     business_name = models.TextField(db_column='BUSINESS_NAME', blank=True, null=True)  # Field name made lowercase.
+    created_by = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
     update_timestamp = models.DateTimeField(auto_now_add=True, blank=False)  # Field name made lowercase.
     latest_in = models.BooleanField(db_column='LATEST_IN', blank=False, default=True) # Field name made lowercase.
 
